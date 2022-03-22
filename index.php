d723fe56293cc38ad0e4a51b7b579b3a9ebf65e9
Eduard4002 <mihiceduard@gmail.com>
2022-03-22 17:57:30 +0100

    Header work
    updated header and css

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="indexStyle.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <div id="mainContent">
+        <div id="mainHeader">
+                <p id="title">stack<b>underflow</b></p>
+        </div>
+    </div>
 </body>
 </html>
