9dcc29fac70ee12115e4cc89ae3ba4935188b0e4
Eduard4002 <mihiceduard@gmail.com>
2022-03-24 21:51:21 +0100

    Added questionsGrid on homepage

---
+++ askQuestion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ask A Question</title>
+    <link rel="stylesheet" href="navStyle.css">
+</head>
+<body>
+    <main>
+        <nav>
+            <p id="title">stack<b>underflow</b></p>
+        </nav>
+    </main>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,16 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="indexStyle.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="navStyle.css">
+    <title>Home</title>
 </head>
 <body>
-    <div id="mainContent">
+    <main>
         <nav>
             <p id="title">stack<b>underflow</b></p>
         </nav>
-    <h2 id="topQuestions">Top Questions</h3>
 
-    <button class="button askButton">Ask A question</button>
-    </div>
+        <h2 id="topQuestionsTitle">Top Questions</h3>
+
+        <a href="askQuestion.php"><button class="button askButton">Ask A question</button></a>
+        
+        <!--Main question div-->
+        <div class="questionsGrid">
+            <div class="questionsChild">
+            </div>
+            <div class="questionsChild">
+            </div>
+            <div class="questionsChild">
+            </div>
+            <div class="questionsChild">
+            </div>
+            
+         
+
+        </div>
+
+
+
+    </main>
 </body>
 </html>
